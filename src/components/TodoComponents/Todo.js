13418dc74b5f3c1a62c624042b5321e166db5992
Danielle B <danibaxx@hotmail.com>
2019-06-05 19:55:31 -0600

    moved array into App.js

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,36 +1,5 @@
 import React from 'react';
 
-const Todo = [
-  {
-    task:'Mow Grass',
-    id: 1,
-    completed: false
-  },
-  {
-    task:'Vacuum',
-    id: 2,
-    completed: false 
-  },
-  {
-    task:'Laundry',
-    id: 3,
-    completed: false
-  },
-  {
-    task:'Dishes',
-    id: 4,
-    completed: false
-  },
-  {
-    task:'Grocery Shopping',
-    id: 5,
-    completed: false
-  },
-  {
-    task:'Clean Bathroom',
-    id: 6,
-    completed: false
-  }
-];
+// map through array here
 
-export default Todo;
+// export default Todo;
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,4 +1,15 @@
 import React from 'react';
 
+const TodoForm = (props) => {
+  return (
+    <div className="to-do-form">
+      <input 
+      type='text'
+      placeholder='...to do' />
+      <button type='submit'>Add To Do</button>
+      <button type='submit'>Clear Completed</button>
+    </div>
+  );
+}
 
 export default TodoForm;
